7a119ed58d150184777c5bfb982ced762658fc76
Shirjeel Alam <shirjeel.alam@7vals.com>
2011-12-11 22:19:03 +0500

    Fix for incorrect redirection after making payment

--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -4,6 +4,6 @@ ActiveAdmin.register Payment do
   member_action :pay, :method => :put do
     payment = Payment.find(params[:id])
     payment.update_attributes(:status => true, :paid_on => Date.today) ? flash[:notice] = 'Payment successfully made.' : flash[:notice] = 'Error in processing payment.'
-    redirect_to :action => :show, :controller => 'admin/enrollments'
+    redirect_to admin_enrollment_path(payment.payable)
   end
 end
