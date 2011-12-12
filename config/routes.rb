b35127c8c3c16b73218ad84481b94eb4e2c5fffc
Shirjeel Alam <shirjeel.alam@7vals.com>
2011-12-12 23:49:39 +0500

    Revert "For heroku deployment"
    This reverts commit 7254e332ae00b5dfe1535bbb80f27270424a7e65.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Criterion::Application.routes.draw do
-  #ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self)
 
-  #devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :admin_users, ActiveAdmin::Devise.config
   
   root :to => 'admin/dashboard#index'
 end
