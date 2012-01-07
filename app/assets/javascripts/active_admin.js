49ae834a8fc0941dde3907067ca5abe874ea4440
Shirjeel Alam <shirjeel.alam@7vals.com>
2012-01-07 17:56:56 +0500

    Added tinymce-rail gem

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,6 +1,7 @@
 //= require active_admin/base
 //= require jquery.purr
 //= require best_in_place
+//= require tinymce-jquery
 
 $(document).ready(function() {
   $(".best_in_place").best_in_place();
