004aab011be2672b9dbe9d97e2d3eca56a3fde96
Shirjeel Alam <shirjeel20@gmail.com>
2012-03-30 16:32:16 +0500

    Small fix

--- app/admin/staffs.rb
+++ app/admin/staffs.rb
@@ -44,7 +44,7 @@ ActiveAdmin.register Staff do
     end
 
     panel 'Payments (Deposits)' do
-      table_for teacher.transactions.debit.each do |t|
+      table_for staff.transactions.debit.each do |t|
         t.column(:id) { |deposit| link_to(deposit.id, admin_payment_path(deposit)) }
         t.column(:payment_date) { |deposit| date_format(deposit.payment_date) }
         t.column(:narration) { |deposit| truncate(deposit.additional_info, :length => 75) }
@@ -54,7 +54,7 @@ ActiveAdmin.register Staff do
     end if staff.transactions.debit.present?
 
     panel 'Payments (Withdrawal)' do
-      table_for teacher.transactions.credit.each do |t|
+      table_for staff.transactions.credit.each do |t|
         t.column(:id) { |withdrawal| link_to(withdrawal.id, admin_payment_path(withdrawal)) }
         t.column(:payment_date) { |withdrawal| date_format(withdrawal.payment_date) }
         t.column(:narration) { |withdrawal| truncate(withdrawal.additional_info, :length => 75) }
