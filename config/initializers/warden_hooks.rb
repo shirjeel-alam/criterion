063f6568524e7ad6a04ff573b1655eb998056ae7
Shirjeel Alam <shirjeel20@gmail.com>
2012-08-28 18:43:17 +0500

    Rescue exceptions in warden

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,7 +1,7 @@
 Warden::Manager.after_set_user do |record, warden, options|
-  record.update_column(:signed_in, true)
+  record.update_column(:signed_in, true) rescue nil
 end
 
 Warden::Manager.before_logout do |record, warden, options|
-  record.update_column(:signed_in, false)
+  record.update_column(:signed_in, false) rescue nil
 end
