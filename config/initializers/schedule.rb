c92cc8e247e0bda71f80d137923eca3a33e9983c
Shirjeel Alam <shirjeel20@gmail.com>
2013-02-16 18:28:27 +0500

    Get date in correct time zone

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -7,12 +7,14 @@ def execute_scheduler
   logger = Logger.new(Rails.root.to_s + '/log/scheduler.log')
 
   scheduler.every '4h' do
-    crd = CriterionReportDate.find_by_report_date(Date.today)
+    date = Time.current.to_date
+
+    crd = CriterionReportDate.find_by_report_date(date)
     if crd.present?
       cr = crd.criterion_report
     else
       cr = CriterionReport.open.first
-      cr.criterion_report_dates.create(report_date: Date.today)
+      cr.criterion_report_dates.create(report_date: date)
     end
     cr.update_report_data
 
@@ -21,8 +23,10 @@ def execute_scheduler
   end
 
   scheduler.every '1w' do
-    CriterionSms.where('created_at < ?', Date.today.advance(weeks: -1).beginning_of_day).destroy_all
-    CriterionMail.where('created_at < ?', Date.today.advance(weeks: -1).beginning_of_day).destroy_all
+    date = Time.current.to_date
+
+    CriterionSms.where('created_at < ?', date.advance(weeks: -1).beginning_of_day).destroy_all
+    CriterionMail.where('created_at < ?', date.advance(weeks: -1).beginning_of_day).destroy_all
   end
 end
 
