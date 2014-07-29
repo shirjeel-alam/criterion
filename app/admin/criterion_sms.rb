1fb334682ec7ca3580cd71925a30e4af7ba0cf16
Shirjeel Alam <shirjeel20@gmail.com>
2014-07-29 10:02:51 +0300

    Admin can see criterion sms page

--- app/admin/criterion_sms.rb
+++ app/admin/criterion_sms.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register CriterionSms do
-	menu parent: 'More Menus', label: 'Criterion SMS', if: proc { current_admin_user.super_admin_or_partner? }
+	menu parent: 'More Menus', label: 'Criterion SMS', if: proc { current_admin_user.super_admin_or_partner? || current_admin_user.admin? }
 
 	filter :id
 	filter :to
@@ -53,7 +53,7 @@ ActiveAdmin.register CriterionSms do
     before_filter :check_authorization
     
     def check_authorization
-      if current_admin_user.all_other?
+      if current_admin_user.all_other? && !current_admin_user.admin?
         if %w[index show edit update destroy].include?(action_name)
           flash[:error] = 'You are not authorized to perform this action'
           redirect_to_back
