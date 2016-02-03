38070f12257c5a88eb1f53bd9e3527ff764892d9
Shirjeel Alam <shirjeel20@gmail.com>
2016-02-03 12:48:01 +0500

    drop delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160203073004) do
+ActiveRecord::Schema.define(:version => 20160203074515) do
 
   create_table "account_entries", :force => true do |t|
     t.integer  "criterion_account_id"
@@ -154,22 +154,6 @@ ActiveRecord::Schema.define(:version => 20160203073004) do
     t.datetime "updated_at",                       :null => false
     t.text     "api_response"
   end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0, :null => false
-    t.integer  "attempts",   :default => 0, :null => false
-    t.text     "handler",                   :null => false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "enrollments", :force => true do |t|
     t.integer  "student_id"
