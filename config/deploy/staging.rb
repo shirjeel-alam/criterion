5c20deaac2b807de6e1519a97d3b6c13dae3fab4
Shirjeel Alam <shirjeel20@gmail.com>
2016-03-08 00:56:51 +0500

    staging server details for capistrano

---
+++ config/deploy/staging.rb
@@ -0,0 +1,6 @@
+set :stage, :production
+set :branch, 'master'
+
+server '198.199.83.180', user: fetch(:user), roles: %w{web app db}, primary: true
+set :deploy_to, "/home/#{fetch(:user)}/rails_apps/#{fetch(:application)}"
+set :rails_env, :production
