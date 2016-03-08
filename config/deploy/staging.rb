ab184f58ac3e626667deb3aa35c9e00c7ce51a4c
Shirjeel Alam <shirjeel20@gmail.com>
2016-03-08 14:46:50 +0500

    use develop branch for staging to test features prior to production

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-set :branch, 'master'
+set :branch, 'develop'
 
 server '198.199.83.180', user: fetch(:user), roles: %w{web app db}, primary: true
 set :deploy_to, "/home/#{fetch(:user)}/rails_apps/#{fetch(:application)}"
