c950496e192f445d394a2de8886f11ddf6d076f5
Shirjeel Alam <shirjeel20@gmail.com>
2016-04-04 15:32:10 +0500

    updates puma configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,25 +12,18 @@ set :ssh_options, {
 set :rbenv_ruby, '2.2.4'
 
 ## puma
-set :puma_threads, [4, 16]
 set :puma_workers, 1
-set :puma_bind, "unix:///tmp/puma.sock"
-set :puma_state, "#{shared_path}/pids/puma.state"
-set :puma_pid, "#{shared_path}/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log, "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
+  # desc 'Restart application'
+  # task :restart do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     invoke 'puma:restart'
+  #   end
+  # end
+
 
   after :finishing, :cleanup
-  after :finishing, :restart
+  # after :finishing, :restart
 end
