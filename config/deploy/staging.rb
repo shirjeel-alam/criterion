dd2b559cb14e547e0b78577dd3661522c8aee1fe
Shirjeel Alam <shirjeel20@gmail.com>
2016-04-04 10:47:41 +0500

    change puma socket location

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,8 @@ set :rails_env, :production
 
 set :puma_threads, [4, 16]
 set :puma_workers, 1
-set :puma_bind, "unix://#{shared_path}/sockets/puma.sock"
+# set :puma_bind, "unix://#{shared_path}/sockets/puma.sock"
+set :puma_bind, "unix:///tmp/puma.sock"
 set :puma_state, "#{shared_path}/pids/puma.state"
 set :puma_pid, "#{shared_path}/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
