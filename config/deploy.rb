b22abd24f3ba6e17f925c6f7465db1a7e34fc200
Shirjeel Alam <shirjeel20@gmail.com>
2016-04-04 12:56:54 +0500

    update capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,24 @@ set :repo_url, 'git@bitbucket.org:shirjeelalam/criterion.git'
 set :scm, :git
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
 set :ssh_options, {
   forward_agent: true
 }
 
+## rbenv
 set :rbenv_ruby, '2.3.0'
+
+## puma
+set :puma_threads, [4, 16]
+set :puma_workers, 1
+set :puma_bind, "unix:///tmp/puma.sock"
+set :puma_state, "#{shared_path}/pids/puma.state"
+set :puma_pid, "#{shared_path}/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
 
 namespace :deploy do
   desc 'Restart application'
